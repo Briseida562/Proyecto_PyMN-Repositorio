0cb3de408060e956130548bfb4ec3d5381b29f57
Briseida562 <75860713+Briseida562@users.noreply.github.com>
2020-12-14 00:33:23 -0800

    Add files via upload
    En este programa identificamos si el usuario es mayor o menor de edad.

---
+++ P2- Mayor o menor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+int edad;
+cout<<"Cual es tu edad?: ";cin>>edad;
+if (edad>=18){
+    cout<<"Eres mayor de edad!";
+}
+
+else if (edad<18){
+    cout <<"Eres menor de edad!";
+}
+
+
+}
+
